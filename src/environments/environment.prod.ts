efba4026ad907e9e9d018753255a68b8c6317fe3
Jassmanni <jassmannq@gmail.com>
2023-01-21 13:12:47 -0400

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'login-app-8eeaf',
+    appId: '1:199528930323:web:1d7aa72eba27cef281f3e6',
+    databaseURL: 'https://login-app-8eeaf.firebaseio.com',
+    storageBucket: 'login-app-8eeaf.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCYG3T--mizCjkrom8FpBe6BLMdjXdOZDU',
+    authDomain: 'login-app-8eeaf.firebaseapp.com',
+    messagingSenderId: '199528930323',
+    measurementId: 'G-ZLX7F63ZM4',
+  },
   production: true
 };
